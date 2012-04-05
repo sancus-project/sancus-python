8dc6870db5d6e8549906198a638d37444fc96da8
Alejandro Mery <amery@geeks.cl>
2012-04-05 15:15:53 +0200

    sancus.web.exc: introduced module exporting webob.exc.*

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from distutils.core import setup
 
+requires = [
+    'webob',
+    ]
+
 setup(
     name='sancus',
     version='0.0dev',
+
     package_dir={'': 'src'},
     packages=['sancus',],
+
+    install_requires = requires,
 )
