2ad48b877c1c8e477f0a3b1477ed6d74ddfcfb48
Alejandro Mery <amery@geeks.cl>
2012-04-06 13:24:28 +0200

    sancus.web.dispatcher: moved __call__() to a generic WSGIMapper class

--- src/sancus/web/dispatcher.py
+++ src/sancus/web/dispatcher.py
@@ -1,5 +1,21 @@
 from webob.exc import HTTPNotFound
 
-class PathMapper(object):
+class WSGIMapper(object):
     def __call__(self, environ, start_response):
+        (h, args) = self.find_handler(environ)
+
+        if 'wsgiorg.routing_args' not in environ:
+            environ['wsgiorg.routing_args'] = ((), {})
+
+        environ['wsgiorg.routing_args'][1].update(args)
+
+        if isinstance(h, type):
+            h = h(environ)
+
+        return h(environ, start_response)
+
+    def find_handler(self, environ):
         raise HTTPNotFound()
+
+class PathMapper(WSGIMapper):
+    pass
