b7d53e4cb21cf3e6ecd813045d18a00fd0251116
Alejandro Mery <amery@geeks.cl>
2012-04-19 15:15:23 +0200

    utils: add Table() class mimicing Lua tables

---
+++ src/sancus/utils.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+class Table(object):
+    """
+    Like a Lua table, where items and attributes are the same thing, and
+    setting them to None deletes them
+    """
+
+    def __init__(self, **kw):
+        # remove None entries, and avoid self._fields recursion
+        d = dict((k,v) for k,v in kw.iteritems() if v is not None)
+        object.__setattr__(self, '_fields', d)
+
+    def __getattr__(self, key):
+        return self._fields.get(self, key, None)
+
+    def __getitem__(self, key):
+        return self._fields.get(self, key, None)
+
+    def __setattr__(self, key, value):
+        if value is not None:
+            self._fields[key] = value
+        else:
+            try:
+                del self._fields[key]
+            except KeyError:
+                pass
+
+    def __setitem__(self, key, value):
+        if value is not None:
+            self._fields[key] = value
+        else:
+            try:
+                del self._fields[key]
+            except KeyError:
+                pass
